4671b0ca44dc812afa19f8677402c568aa1f5409
Sabuhi0 <sabuhiq0@gmail.com>
2022-01-20 18:34:59 -0500

    Add aside secton

--- NamingConventionInAction/src/assets/js/main.js
+++ NamingConventionInAction/src/assets/js/main.js
@@ -1,1 +1,29 @@
-console.log("Hello")
+
+/* === Search-Form === */
+
+// Add
+document.querySelector(".search-btn").addEventListener("click", function() {
+    const headerSearchForm = document.querySelector(".header-search-form");
+    headerSearchForm.classList.add("active")
+});
+
+// Remove
+document.querySelector(".search-close").addEventListener("click", function() {
+    const headerSearchForm = document.querySelector(".header-search-form");
+    headerSearchForm.classList.remove("active")
+});
+
+//* === Aside-Section === */
+
+// Add
+document.querySelector(".aside-btn").addEventListener("click", function() {
+    const headerSearchForm = document.querySelector(".aside-items");
+    headerSearchForm.classList.add("active")
+});
+
+// Remove
+document.querySelector(".nav-aside-close").addEventListener("click", function() {
+    const headerSearchForm = document.querySelector(".aside-items");
+    headerSearchForm.classList.remove("active")
+});
+
