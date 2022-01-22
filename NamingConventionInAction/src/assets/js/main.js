39df387c8508dde4102f000f138370f1c3094f54
Sabuhi0 <sabuhiq0@gmail.com>
2022-01-22 20:21:29 +0400

    Add recent post section and update aside section

--- NamingConventionInAction/src/assets/js/main.js
+++ NamingConventionInAction/src/assets/js/main.js
@@ -15,17 +15,23 @@ document.querySelector(".search-close").addEventListener("click", function() {
 
 //* === Aside-Section === */
 
-// Add
+// Click-Button Aside Add
 document.querySelector(".aside-btn").addEventListener("click", function() {
     const headerSearchForm = document.querySelector(".aside-items");
     headerSearchForm.classList.add("active")
 });
 
-// Remove
+// Click-Button Aside Remove
 document.querySelector(".nav-aside-close").addEventListener("click", function() {
     const headerSearchForm = document.querySelector(".aside-items");
     headerSearchForm.classList.remove("active")
 });
+
+// Aside Remove
+const headerSearchForm = document.querySelector("#nav-aside")
+headerSearchForm.addEventListener("click",(e)=>{
+    headerSearchForm.classList.remove("active")
+})
 
 //* === Shadow === */
 
@@ -33,6 +39,7 @@ document.querySelector(".nav-aside-close").addEventListener("click", function() 
 document.querySelector(".aside-btn").addEventListener("click", function() {
     const headerSearchForm = document.querySelector("#nav-aside");
     headerSearchForm.classList.add("active")
+    headerSearchForm.style.backgroundColor="rgba(33,38,49,.5)"
 });
 
 // Remove
@@ -40,5 +47,3 @@ document.querySelector(".nav-aside-close").addEventListener("click", function() 
     const headerSearchForm = document.querySelector("#nav-aside");
     headerSearchForm.classList.remove("active")
 });
-
-
