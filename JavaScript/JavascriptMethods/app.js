82c7c9aeee9786b68ec700d54a520dab87beaba9
Sabuhi0 <sabuhiq0@gmail.com>
2022-02-22 19:12:00 +0400

    Add example filter method

--- JavaScript/JavascriptMethods/app.js
+++ JavaScript/JavascriptMethods/app.js
@@ -1,1 +1,35 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+// Filter metodu
+
+const filterNumber = [30, 25, 22, 18, 45, 60, 70];
+
+const filteredNumber = filterNumber.filter(age => {
+    return age > 45
+})
+
+console.log(filteredNumber) // 60, 70
+
+/* ======================================== */
+
+const filterArray = ["JavaScript", "React", "Sass", "Python"];
+
+const filteredArray = filterArray.filter(lang => {
+    return lang.length < 6
+})
+
+console.log(filteredArray) // [ 'React', 'Sass' ]
+
+/* ========================================= */
+
+const data = [
+    { name: "Apple", price: 2500 },
+    { name: "Samsung", price: 2000 },
+    { name: "Sony", price: 1600 }
+];
+
+const filteredData = data.filter(item => {
+    return item.price < 2200;
+})
+
+console.log(filteredData); // [ { name: 'Samsung', price: 2000 }, { name: 'Sony', price: 1600 } ]
