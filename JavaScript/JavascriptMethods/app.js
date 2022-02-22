e61dc0f7f1067a96f04ccb91d0b840bcdaa17def
Sabuhi0 <sabuhiq0@gmail.com>
2022-02-22 21:40:23 +0400

    Add map,find,forEach,some

--- JavaScript/JavascriptMethods/app.js
+++ JavaScript/JavascriptMethods/app.js
@@ -33,3 +33,63 @@ const filteredData = data.filter(item => {
 })
 
 console.log(filteredData); // [ { name: 'Samsung', price: 2000 }, { name: 'Sony', price: 1600 } ]
+
+
+// Map metodu
+
+const births = [1994, 2004, 2000, 1998];
+
+const ages = births.map(x => 2022 - x);
+console.log(ages) // 28, 18, 22, 24
+
+
+// Find metodu
+
+const persons = [
+    { name: "Sabuhi", age: 17 },
+    { name: "Rahman", age: 27 },
+    { name: "Habib", age: 20 },
+]
+
+const person = persons.find(person => {
+    return person.name === "Sabuhi"
+})
+
+console.log(person);
+
+
+// ForEach metodu
+
+const humans = [
+    { name: "Sabuhi", age: 17 },
+    { name: "Rahman", age: 27 },
+    { name: "Habib", age: 20 },
+]
+
+const human = humans.forEach(human => {
+    console.log(human.name); // Sabuhi Rahman Habib
+})
+
+console.log("------------------")
+
+// For
+
+for (let i = 0; i < humans.length; i++) {
+    console.log(humans[i].name); // Sabuhi Rahman Habib
+}
+
+
+// Some metodu
+
+const num = [23, 13, 55, 14, 36];
+
+const falseSome = num.some(element => {
+    return element > 60;
+})
+
+const trueSome = num.some(element => {
+    return element % 3 === 0;
+})
+
+console.log(falseSome);
+console.log(trueSome);
