9e114d20e6f825b6137c16fe1a6f9dde640817cf
Sabuhi0 <sabuhiq0@gmail.com>
2022-02-25 21:24:22 +0400

    Add example callback hell

---
+++ JavaScript/JavascriptPromise/assest/js/main.js
@@ -0,0 +1,26 @@
+const box = document.querySelector(".box");
+
+setTimeout(() => {
+    box.classList.add("bigger")
+    setTimeout(() => {
+        box.classList.add("circle")
+        setTimeout(() => {
+            box.classList.add("move")
+            setTimeout(() => {
+                box.classList.add("colored")
+                setTimeout(() => {
+                    box.classList.remove("colored")
+                    setTimeout(() => {
+                        box.classList.remove("move")
+                        setTimeout(() => {
+                            box.classList.remove("circle")
+                            setTimeout(() => {
+                                box.classList.remove("bigger")
+                            }, 500)
+                        }, 500)
+                    }, 500)
+                }, 500)
+            }, 500)
+        }, 500)
+    }, 500)
+}, 500)
