fde8b8c32b3f4428de275d5d02fbe37729efd886
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-04 16:56:27 +0400

    Initial Component Creation

--- LearnReact/reactapp/src/App.js
+++ LearnReact/reactapp/src/App.js
@@ -1,18 +1,12 @@
+import User from './components/User';
 import './App.css';
 
 function App() {
-  const num = 5;
-  const isAuth = true;
   return (
     <div className="App">
-      <h1>{3+4}</h1>
-      <h2>{"sabuhi".toUpperCase()}</h2>
-      <p>{num}</p>
-      <h3>
-      {
-        isAuth ? <p>İstifadəçi qeydiyyatdan keçib</p>: <span>Qeydiyyatdan keç.</span>
-      }
-      </h3>
+      <h1>App Component</h1>
+      <User/>
+      <User/>
     </div>
   );
 }
