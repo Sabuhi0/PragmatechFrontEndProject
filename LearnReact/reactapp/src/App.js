b465e4491a553b16bb7d692281a63c9f53b4c1f5
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-04 16:16:35 +0400

    JSX and Javascript Expressions

--- LearnReact/reactapp/src/App.js
+++ LearnReact/reactapp/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 
 function App() {
+  const num = 5;
+  const isAuth = true;
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h1 className = "text">Blah blah blah</h1>
+      <h1>{3+4}</h1>
+      <h2>{"sabuhi".toUpperCase()}</h2>
+      <p>{num}</p>
+      <h3>
+      {
+        isAuth ? <p>İstifadəçi qeydiyyatdan keçib</p>: <span>Qeydiyyatdan keç.</span>
+      }
+      </h3>
     </div>
   );
 }
 
-export default App;
+export default App; 
