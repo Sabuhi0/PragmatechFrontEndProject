38825aa958fe090266f50457dbb945f70fd97733
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-06 03:04:56 +0400

    Add simple comment project

--- LearnReact/blog-posts/src/index.js
+++ LearnReact/blog-posts/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SingleComment from './SingleComment';
+import profile1 from './images/pic-1.jpeg'
+import profile2 from './images/pic-2.jpeg'
+import profile3 from './images/pic-3.jpeg'
 
 const App = () => {
     return(
-        <div style={{fontSize:'20px',color:'red'}}>Hello World!</div>
+        <div className='ui comments'>
+            <SingleComment 
+                name= 'Ravi' 
+                date= 'Today at 5:00' 
+                text= 'it is amazing'
+                image= {profile2}
+            />
+            <SingleComment 
+                name= 'Yusif' 
+                date='Today at 6:00' 
+                text= 'Great Jop'
+                image= {profile3}
+            />
+            <SingleComment 
+                name= 'Huseyn' 
+                date='Today at 7:00' 
+                text= 'Thanks...'
+                image= {profile1}
+            />
+        </div>
     )
 }
 
