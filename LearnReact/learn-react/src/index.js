f654261d2993d03d44c164ca23ab872c066b7455
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-05 15:23:44 +0400

    Add Function Component Creation

--- LearnReact/learn-react/src/index.js
+++ LearnReact/learn-react/src/index.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
+// Normal function ile
+// const App = function() {
+//     return(
+//         <div>Hello World!</div>
+//     )
+// }
+
+// Arrow function ile
+const App = () => {
+    return(
+        <div>Hello World!</div>
+    )
+}
+
+ReactDom.render(
+    <App/>,
+    document.querySelector("#root")
+)
