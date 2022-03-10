e08cf923fe9d9dff295927805387a914efe35724
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-10 16:37:55 +0400

    Add user card jsx

--- LearnReact/blog-posts/src/UserCard.jsx
+++ LearnReact/blog-posts/src/UserCard.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const UserCard = () => {
     return (
-        <div>
-            <h1>Card</h1>
+        <div className="ui card">
+            <div className="content">
+                <div className="header">
+                    Alex Tancredi
+                </div>
+                <div className="description">
+                    Hello my name is Alex, I live Baku
+                </div>
+            </div>
+            <div className="ui bottom button">
+                <i className="add icon"></i>
+                Add Friend
+            </div>
         </div>
     )
 }
