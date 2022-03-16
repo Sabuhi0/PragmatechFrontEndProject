e44e2ce174df13941ee669e5c4152393827ebc27
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-17 02:14:17 +0400

    Add images-list project

--- LearnReact/images-list/src/components/App.jsx
+++ LearnReact/images-list/src/components/App.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Search from "./Search";
 import axios from 'axios'
+import ImageList from "./ImagesList";
 
 class App extends React.Component {
-    onSearchSubmit = (entry) => {
-        console.log(entry)
+
+    state = { images: [] }
+
+    onSearchSubmit = async (entry) => {
+        const response = await axios.get(`https://pixabay.com/api/?key=21115300-b484d5aa3a5ce17fbbdeef09b&q=${entry}&image_type=photo`);
+        this.setState({images:response.data.hits})
     }
+
     render() {
         return(
             <div className="ui container" style={{marginTop:'30px'}}> 
                 <Search onSearchSubmit={this.onSearchSubmit}/>
+                <ImageList images={this.state.images} />
             </div>
         )
     }
--- LearnReact/images-list/src/components/ImagesList.jsx
+++ LearnReact/images-list/src/components/ImagesList.jsx
@@ -1,1 +1,18 @@
 import React from "react";
+import '../css/app.css'
+
+const ImageList = (props) => {
+    // console.log(props)
+    const images = props.images.map(image => {
+        return (
+            <img className="img" key={image.id} src={image.webformatURL}/>
+        )
+    });
+    return (
+        <div className="grid">
+            {images}
+        </div>
+    )
+}
+
+export default ImageList;
