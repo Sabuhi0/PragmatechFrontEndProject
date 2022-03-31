74bdb44baab8586440c025db684862f42dea56f1
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-31 19:51:44 +0400

    Add about component

--- LearnReact/react-router/src/components/Home.jsx
+++ LearnReact/react-router/src/components/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='ui raised very padded text container segment' style={{marginTop: '80px'}}>
+      <h3 className='ui header'>Home</h3>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
     </div>
   )
 }
 
-export default Home
+export default Home;
