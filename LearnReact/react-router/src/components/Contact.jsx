8ca6dbd6fd994b69e628feb194daf4bb20209e8d
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-01 23:55:59 +0400

    Add react-router

--- LearnReact/react-router/src/components/Contact.jsx
+++ LearnReact/react-router/src/components/Contact.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Contact = () => {
-    return (
-      <div className='ui raised very padded text container segment' style={{marginTop: '80px'}}>
-        <h3 className='ui header'>Contact</h3>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
-      </div>
-    )
-  }
+  return (
+    <div className='ui raised very padded text container segment' style={{marginTop: '80px'}}>
+      <h3 className='ui header'>Contact</h3>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+    </div>
+  )
+}
   
 export default Contact;
   
