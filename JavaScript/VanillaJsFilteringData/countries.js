d30ff78bcea9bda6206ac5d8d9639f299a9c9f59
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-14 01:07:24 +0400

    Update

---
+++ JavaScript/VanillaJsFilteringData/countries.js
@@ -0,0 +1,32 @@
+countries = {
+    Afghanistan: [
+        "Herat",
+        "Kabul",
+        "Kandahar",
+        "Molah",
+        "Rana",
+        "Shar",
+        "Sharif",
+        "Wazir Akbar Khan"
+    ],
+    Albania: [
+        "Elbasan",
+        "Petran",
+        "Pogradec",
+        "Shkoder",
+        "Tirana",
+        "Ura Vajgurore"
+    ],
+}
+
+countries.Albania.forEach(element => {
+    if(element.length === 13) {
+        console.log(element)
+    }
+});
+
+function countryNames () {
+    
+}
+
+countryNames()
