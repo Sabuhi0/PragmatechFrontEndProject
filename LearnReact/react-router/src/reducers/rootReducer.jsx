241599ee97a82fb2ffbf2faaf920382d6695b661
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-13 18:20:38 +0400

    Add reducers

--- LearnReact/react-router/src/reducers/rootReducer.jsx
+++ LearnReact/react-router/src/reducers/rootReducer.jsx
@@ -1,5 +1,21 @@
 const initialState = {
-    cards:[]
+    cards:[
+        {
+            id:'1',
+            title: 'Alex',
+            body:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+        },
+        {
+            id:'2',
+            title: 'John',
+            body:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+        },
+        {
+            id:'3',
+            title: 'Willma',
+            body:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+        }
+    ]
 }
 
 const rootReducer = (state = initialState, action) => {
