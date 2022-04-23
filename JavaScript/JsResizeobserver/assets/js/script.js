6871a92dc483eeb965e06bf03a9e1c7195c3ad1a
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-23 23:53:49 +0400

    Simple Resize observer api project

--- JavaScript/JsResizeobserver/assets/js/script.js
+++ JavaScript/JsResizeobserver/assets/js/script.js
@@ -1,12 +1,32 @@
-const textareas = document.querySelectorAll('textarea');
+// const textareas = document.querySelectorAll('textarea');
 
-const resizeObserver = new ResizeObserver(entries => {
-    for( let entry of entries) {
-        // console.log("Height: "+ entry.contentRect.height)
-        const size = entry.contentRect.height / 3 - 1;
-        entry.target.style.fontSize = size + 'px'
-        entry.target.style.lineHeight = size + 'px'
-    }
+// const resizeObserver = new ResizeObserver(entries => {
+//     for( let entry of entries) {
+//         // console.log("Height: "+ entry.contentRect.height)
+//         const size = entry.contentRect.height / 3 - 1;
+//         entry.target.style.fontSize = size + 'px'
+//         entry.target.style.lineHeight = size + 'px'
+//     }
+// })
+
+// textareas.forEach(textarea => resizeObserver.observe(textarea))
+
+
+const resizeObserver = new ResizeObserver(([entry]) => {
+    // const boxWidth = entry.target.querySelector('.box').offsetWidth;
+    const visibles = Math.floor(entry.contentRect.width / 200);
+    
+    entry.target.querySelectorAll(".box").forEach((box, index) => {
+        if( (index + 1) <= visibles) {
+            // goster
+            box.style.display = 'flex'
+        }
+        else {
+            // gosterme
+            box.style.display = 'none'
+        }
+    })
+
 })
 
-textareas.forEach(textarea => resizeObserver.observe(textarea))
+resizeObserver.observe(document.querySelector(".boxes"))
