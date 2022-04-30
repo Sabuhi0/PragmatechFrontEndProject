988744bd5b43606dab8669b23a78830673473c13
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-01 00:46:14 +0400

    Initial navbar component

---
+++ LearnReact/contextapi/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav>
+            <h2>Initial navbar Component</h2>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
