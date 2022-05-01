c3244cc604362ee66e848a4e8015104bad10465c
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-02 00:09:43 +0400

    Update app.js

--- JavaScript/JsIntersectionObserver/assets/js/app.js
+++ JavaScript/JsIntersectionObserver/assets/js/app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+// console.log("Hello World!")
+
+const target = document.getElementById("box");
+
+const callback = (entries) => {
+    console.log(entries)    
+}
+
+const options = {
+    threshold: [0.2, 0.5, 0.8],
+}
+
+const observer = new IntersectionObserver(callback, options);
+observer.observe(target)
