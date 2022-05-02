d034a852ae6e30e01616c52f8079d9956508de34
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-02 17:03:44 +0400

    Intersection observer

--- JavaScript/JsIntersectionObserver/assets/js/app.js
+++ JavaScript/JsIntersectionObserver/assets/js/app.js
@@ -1,13 +1,16 @@
 // console.log("Hello World!")
 
-const target = document.getElementById("box");
+const target = document.getElementById("image");
 
 const callback = (entries) => {
-    console.log(entries)    
+    if(entries[0].isIntersecting) {
+        entries[0].target.src = entries[0].target.dataset.src  
+        console.log(entries[0])
+    }  
 }
 
 const options = {
-    threshold: [0.2, 0.5, 0.8],
+    rootMargin: "-100px",
 }
 
 const observer = new IntersectionObserver(callback, options);
