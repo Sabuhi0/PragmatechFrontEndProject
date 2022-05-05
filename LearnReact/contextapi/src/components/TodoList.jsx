0c5c58047c2a0866ca36a8f5cd1d4300661b4c36
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-05 22:43:59 +0400

    Let's Access the Data in Context

--- LearnReact/contextapi/src/components/TodoList.jsx
+++ LearnReact/contextapi/src/components/TodoList.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class TodoList extends React.Component {
-  render() {
-    return (
-      <div className='ui list'>
-        <p className='item'>Plan the family</p>
-        <p className='item'>Go for shopping for dinner</p>
-        <p className='item'>Go for a walk</p>
-      </div>
-    )
-  }
+    static contextType = ThemeContext
+    render() {
+        const { isDarkTheme, lightTheme, darkTheme } = this.context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+        return (
+            <div style={{background: theme.background, color: theme.text, height: '120px', textAlign: 'center', justifyContent: 'center'}}>
+                <p className='item'>Plan the family</p>
+                <p className='item'>Go for shopping for dinner</p>
+                <p className='item'>Go for a walk</p>
+            </div>
+        )
+    }
 }
 
 export default TodoList;
