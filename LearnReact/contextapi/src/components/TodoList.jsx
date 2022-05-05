a9ded52176967051c949bcc21eeaaaf94925c9e1
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-05 15:11:53 +0400

    Add Semantic-UI ToDoList component

--- LearnReact/contextapi/src/components/TodoList.jsx
+++ LearnReact/contextapi/src/components/TodoList.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 class TodoList extends React.Component {
   render() {
     return (
-      <div>
-        
+      <div className='ui list'>
+        <p className='item'>Plan the family</p>
+        <p className='item'>Go for shopping for dinner</p>
+        <p className='item'>Go for a walk</p>
       </div>
     )
   }
