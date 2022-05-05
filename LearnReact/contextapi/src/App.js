00b0ec0b3c0abf4fecfbd97f5f0d365b14197dd2
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-05 15:14:05 +0400

    Import ToDoList component

--- LearnReact/contextapi/src/App.js
+++ LearnReact/contextapi/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
         <Navbar/>
+        <TodoList/>
       </div>
     </div>
   );
