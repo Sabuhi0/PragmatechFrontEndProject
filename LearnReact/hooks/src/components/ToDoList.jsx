9bb9267ceb214b8b8fd6e445ecb1b5c79bd1c381
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-06 19:22:32 +0400

    Add to do list functiom component

---
+++ LearnReact/hooks/src/components/ToDoList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ToDoList = () => {
+    return(
+        <div>
+            <ul>
+                <li>Pay bills</li>
+                <li>Do your homework</li>
+                <li>Feed the dog</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ToDoList;
