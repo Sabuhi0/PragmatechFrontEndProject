d40680a4fd298d5d44eeddd8d78173a151f1000d
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-06 16:58:50 +0400

    Update

--- LearnReact/contextapi/src/App.js
+++ LearnReact/contextapi/src/App.js
@@ -1,14 +1,17 @@
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
         <ThemeContextProvider>
-          <Navbar/>
-          <TodoList/>
+          <AuthContextProvider>
+            <Navbar/>
+            <TodoList/>
+          </AuthContextProvider>
         </ThemeContextProvider>
       </div>
     </div>
