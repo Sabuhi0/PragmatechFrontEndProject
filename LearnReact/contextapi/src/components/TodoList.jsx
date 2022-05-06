18db64d45ceab958b8d25df0b414347278baa79e
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-06 14:09:13 +0400

    Change theme

--- LearnReact/contextapi/src/components/TodoList.jsx
+++ LearnReact/contextapi/src/components/TodoList.jsx
@@ -4,13 +4,14 @@ import { ThemeContext } from '../contexts/ThemeContext';
 class TodoList extends React.Component {
     static contextType = ThemeContext
     render() {
-        const { isDarkTheme, lightTheme, darkTheme } = this.context;
+        const { isDarkTheme, lightTheme, darkTheme, changeTheme } = this.context;
         const theme = isDarkTheme ? darkTheme : lightTheme;
         return (
-            <div style={{background: theme.background, color: theme.text, height: '120px', textAlign: 'center', justifyContent: 'center'}}>
+            <div style={{background: theme.background, color: theme.text, height: '120px', textAlign: 'center'}}>
                 <p className='item'>Plan the family</p>
                 <p className='item'>Go for shopping for dinner</p>
                 <p className='item'>Go for a walk</p>
+                <button className='ui button primary' onClick={changeTheme} style={{marginTop:'8px'}}>Change the theme</button>
             </div>
         )
     }
