13c98962e6bd644a328fc5a7d52b2e359d2e95c4
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-06 17:25:34 +0400

    Using multiple contexts

--- LearnReact/contextapi/src/contexts/AuthContext.jsx
+++ LearnReact/contextapi/src/contexts/AuthContext.jsx
@@ -3,10 +3,19 @@ import React, {createContext} from "react";
 export const AuthContext = createContext();
 
 class AuthContextProvider extends React.Component {
+    state =  {
+        isLoggedIn: false,
+    };
+
+    changeAuthStatus = () => {
+        this.setState({ isLoggedIn: !this.state.isLoggedIn })
+    }
 
     render() {
         return(
-            'Jsx'
+            <AuthContext.Provider value={{ ...this.state, changeAuthStatus:this.changeAuthStatus }} >
+                { this.props.children }
+            </AuthContext.Provider>
         )
     }
 }
