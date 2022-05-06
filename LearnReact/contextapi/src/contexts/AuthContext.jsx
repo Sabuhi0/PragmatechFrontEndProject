69d9b8352da977cd065a122d3617da7880716ec8
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-06 14:20:58 +0400

    Initial context

---
+++ LearnReact/contextapi/src/contexts/AuthContext.jsx
@@ -0,0 +1,14 @@
+import React, {createContext} from "react";
+
+export const AuthContext = createContext();
+
+class AuthContextProvider extends React.Component {
+
+    render() {
+        return(
+            'Jsx'
+        )
+    }
+}
+
+export default AuthContextProvider;
