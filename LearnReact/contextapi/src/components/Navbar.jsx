00dae64060316ac6e761d8ae2386fa372988f8d7
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-06 17:25:06 +0400

    Using multiple contexts

--- LearnReact/contextapi/src/components/Navbar.jsx
+++ LearnReact/contextapi/src/components/Navbar.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends React.Component {
-  static contextType = ThemeContext
+  // static contextType = ThemeContext
   render() {
     return (
-      <ThemeContext.Consumer>{(context) => {
-        const { isDarkTheme, lightTheme, darkTheme } = context;
-        const theme = isDarkTheme ? darkTheme : lightTheme
-        return(
-        <div>
-          <nav style={{ background: theme.background, color: theme.text, height: '120px' }}>
-            <h2 style={{ textAlign: 'center' }}>Pragmatech</h2>
-            <div className='ui three buttons'>
-              <button className='ui button'>Overview</button>
-              <button className='ui button'>Contact</button>
-              <button className='ui button'>Support</button>
+      <AuthContext.Consumer>{(authContext) => (
+        <ThemeContext.Consumer>{(themeContext) => {
+          const { isDarkTheme, lightTheme, darkTheme } = themeContext;
+          const { isLoogedIn, changeAuthStatus } = authContext
+          const theme = isDarkTheme ? darkTheme : lightTheme
+          return(
+            <div>
+              <nav style={{ background: theme.background, color: theme.text, height: '120px' }}>
+                <h2 style={{ textAlign: 'center' }}>Pragmatech</h2>
+                <p style={{textAlign: 'center'}} onClick={changeAuthStatus}> { isLoogedIn ? "Login": 'Log out' }</p>
+                <div className='ui three buttons'>
+                  <button className='ui button'>Overview</button>
+                  <button className='ui button'>Contact</button>
+                  <button className='ui button'>Support</button>
+                </div>
+              </nav>
             </div>
-          </nav>
-        </div>
-      )
-      }}
-      </ThemeContext.Consumer>
+          )
+        }}
+        </ThemeContext.Consumer>
+      )}
+      </AuthContext.Consumer>
+      
     )
   }
 }
