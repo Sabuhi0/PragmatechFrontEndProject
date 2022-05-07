5ed22472f99c81a2253958d53955c360f32eb6ff
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-07 22:25:15 +0400

    Update

--- LearnReact/contextapi/src/components/TodoList.jsx
+++ LearnReact/contextapi/src/components/TodoList.jsx
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
+import { TodoListContext } from '../contexts/ToDoListContext';
 
 const TodoList = () => {
     const { isDarkTheme, lightTheme, darkTheme, changeTheme } = useContext(ThemeContext);
+    const {todos} =  useContext(TodoListContext);
     const theme = isDarkTheme ? darkTheme : lightTheme;
     return (
         <div style={{background: theme.background, color: theme.text, height: '120px', textAlign: 'center'}}>
-            <p className='item'>Plan the family</p>
-            <p className='item'>Go shopping for dinner</p>
-            <p className='item'>Go for a walk</p>
+            {
+                todos.length ? (
+                    todos.map(todo => {
+                        return <p className='item' key={todo.id}>{todo.text}</p>  
+                    })
+                ) : (
+                    <h1>You have no todos</h1>
+                )
+            }
             <button className='ui button primary' onClick={changeTheme} style={{marginTop:'8px'}}>Change the theme</button>
         </div>
     )
