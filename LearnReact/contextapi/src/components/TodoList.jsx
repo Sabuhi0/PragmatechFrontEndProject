aed3240c440f9712a62d0c9502a02ae7140787cf
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-07 20:17:57 +0400

    Remove txt file

--- LearnReact/contextapi/src/components/TodoList.jsx
+++ LearnReact/contextapi/src/components/TodoList.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class TodoList extends React.Component {
-    static contextType = ThemeContext
-    render() {
-        const { isDarkTheme, lightTheme, darkTheme, changeTheme } = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-        return (
-            <div style={{background: theme.background, color: theme.text, height: '120px', textAlign: 'center'}}>
-                <p className='item'>Plan the family</p>
-                <p className='item'>Go shopping for dinner</p>
-                <p className='item'>Go for a walk</p>
-                <button className='ui button primary' onClick={changeTheme} style={{marginTop:'8px'}}>Change the theme</button>
-            </div>
-        )
-    }
+const TodoList = () => {
+    const { isDarkTheme, lightTheme, darkTheme, changeTheme } = useContext(ThemeContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return (
+        <div style={{background: theme.background, color: theme.text, height: '120px', textAlign: 'center'}}>
+            <p className='item'>Plan the family</p>
+            <p className='item'>Go shopping for dinner</p>
+            <p className='item'>Go for a walk</p>
+            <button className='ui button primary' onClick={changeTheme} style={{marginTop:'8px'}}>Change the theme</button>
+        </div>
+    )
 }
 
 export default TodoList;
