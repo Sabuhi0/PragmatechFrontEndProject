beee817b6640059af280ff5232cc74a2a1e5d9b2
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-07 18:36:17 +0400

    Add required attribute

--- LearnReact/hooks/src/components/AddNewTodo.jsx
+++ LearnReact/hooks/src/components/AddNewTodo.jsx
@@ -11,7 +11,7 @@ const AddNewTodo = ({addTodo}) => {
     return(
         <form onSubmit={onFormSubmit}>
             <label htmlFor="todo"></label>
-            <input type="text" id="todo" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <input type="text" id="todo" value={todo} onChange={(e) => setTodo(e.target.value)} required/>
             <input type="submit" />
         </form>
     );
