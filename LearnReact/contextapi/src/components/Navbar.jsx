0d8572511bc68ebfed9cece75bb816ed6d456dc7
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-07 20:20:02 +0400

    useState

--- LearnReact/contextapi/src/components/Navbar.jsx
+++ LearnReact/contextapi/src/components/Navbar.jsx
@@ -1,36 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
-class Navbar extends React.Component {
-  // static contextType = ThemeContext
-  render() {
-    return (
-      <AuthContext.Consumer>{(authContext) => (
-        <ThemeContext.Consumer>{(themeContext) => {
-          const { isDarkTheme, lightTheme, darkTheme } = themeContext;
-          const { isLoogedIn, changeAuthStatus } = authContext
-          const theme = isDarkTheme ? darkTheme : lightTheme
-          return(
-            <div>
-              <nav style={{ background: theme.background, color: theme.text, height: '120px' }}>
-                <h2 style={{ textAlign: 'center' }}>Pragmatech</h2>
-                <p style={{textAlign: 'center'}} onClick={changeAuthStatus}> { isLoogedIn ? "Login": 'Log out' }</p>
-                <div className='ui three buttons'>
-                  <button className='ui button'>Overview</button>
-                  <button className='ui button'>Contact</button>
-                  <button className='ui button'>Support</button>
-                </div>
-              </nav>
-            </div>
-          )
-        }}
-        </ThemeContext.Consumer>
-      )}
-      </AuthContext.Consumer>
-      
-    )
-  }
+const Navbar = () => {
+  const { isDarkTheme, lightTheme, darkTheme } = useContext(ThemeContext);
+  const { isLoogedIn, changeAuthStatus } = useContext(AuthContext)
+  const theme = isDarkTheme ? darkTheme : lightTheme
+  return(
+    <nav style={{ background: theme.background, color: theme.text, height: '120px' }}>
+      <h2 style={{ textAlign: 'center' }}>Pragmatech</h2>
+      <p style={{textAlign: 'center'}} onClick={changeAuthStatus}> { isLoogedIn ? "Login": 'Log out' }</p>
+      <div className='ui three buttons'>
+        <button className='ui button'>Overview</button>
+        <button className='ui button'>Contact</button>
+        <button className='ui button'>Support</button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
