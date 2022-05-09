2a1dc0b600686b304e4a73a3d6fdd9efd64513ab
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-09 19:33:30 +0400

    Close code

--- LearnReact/contextapi/src/contexts/ToDoListContext.jsx
+++ LearnReact/contextapi/src/contexts/ToDoListContext.jsx
@@ -3,7 +3,7 @@ import React, {createContext, useState} from 'react';
 export const TodoListContext = createContext();
 
 const TodoListContextProvider = ({ children }) => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
 
     const addTodo = (todo) => {
         setTodos([
