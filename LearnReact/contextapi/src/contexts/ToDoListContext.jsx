a61d5a8478958cae6e4f0c90f8cc6f17d52b3fe4
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-09 19:30:47 +0400

    Add add todo and remove todo

--- LearnReact/contextapi/src/contexts/ToDoListContext.jsx
+++ LearnReact/contextapi/src/contexts/ToDoListContext.jsx
@@ -3,13 +3,25 @@ import React, {createContext, useState} from 'react';
 export const TodoListContext = createContext();
 
 const TodoListContextProvider = ({ children }) => {
-    const [todos, setTodos] = useState([
-        {text: 'Plan the family', id:1},
-        {text: 'Go shopping for dinner', id:2},
-        {text: 'Go for a walk', id:3}
-    ])
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (todo) => {
+        setTodos([
+            ...todos,
+            {
+                id: Math.random(),
+                text: todo
+            }
+        ])
+    };
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter(todo => {
+            return todo.id !== Number(id)
+        }));
+    }
     return(
-        <TodoListContext.Provider value={{ todos }}>
+        <TodoListContext.Provider value={{ todos, addTodo, removeTodo }}>
             { children }
         </TodoListContext.Provider>
     )
