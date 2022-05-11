4919e32d0521bf9929ae596eb7ed9b594957a2ba
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-11 20:41:45 +0400

    Inersection observer

--- JavaScript/JsIntersectionObserver/assets/js/app.js
+++ JavaScript/JsIntersectionObserver/assets/js/app.js
@@ -1,15 +1,32 @@
-const target = document.getElementById("image");
+const targets = document.querySelectorAll(".box");
 
 const callback = (entries) => {
-    if(entries[0].isIntersecting) {
-        entries[0].target.src = entries[0].target.dataset.src  
-        console.log(entries[0])
-    }  
-}
+    for(const entry of entries) {
+        for(const entry of entries) {
+            // entry.target.classList.toggle("active", entry.isIntersecting);
+            // console.log(entries)
+            entry.target.querySelector('.image').style.transform = 'translateX(' + -(100 - entry.intersectionRatio * 100) + '%)';
+            entry.target.querySelector('.image').style.opacity =  entry.intersectionRatio;
+            
+            entry.target.querySelector('.text').style.transform = 'scale(' + entry.intersectionRatio + ')';
+            entry.target.querySelector('.text').style.opacity =  entry.intersectionRatio;
+        }
+        // if(entry.isIntersecting) {
+        //     entry.target.classList.add("active");
+        // }
+        // else {
+        //     entry.target.classList.remove("active");
+        // }
+    }
+};
+
+let keys = [...Array(100).keys()].map((key) => '0.' + (key < 10 ? '0' + key : key ));
+ 
+// console.log(keys)
 
 const options = {
-    rootMargin: "-150px",
+    threshold: keys,
 }
 
 const observer = new IntersectionObserver(callback, options);
-observer.observe(target)
+targets.forEach((box) => observer.observe(box));
