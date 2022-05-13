8efc1c6e87588613005ea5b5ca50eda9bab2e3ff
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-13 18:56:40 +0400

    Initial state

---
+++ LearnReact/to-do-app/src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+
+const TodoList = () => {
+    const [todos, setTodos] = useState([]);
+
+    return (
+        <div>
+            <h1>What's the plan for Todoy?</h1>
+        </div>
+    )
+}
+
+export default TodoList
