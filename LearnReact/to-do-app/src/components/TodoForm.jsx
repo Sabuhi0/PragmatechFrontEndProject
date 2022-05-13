b9faf49f64bf52cf7fa6b35f6fe4f3684e070db5
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-13 15:21:05 +0400

    Initial form state

---
+++ LearnReact/to-do-app/src/components/TodoForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const TodoForm = () => {
+    const [input, setInput] = useState('')
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TodoForm
