bdf81c2f1f06ee84bd1d5e6fb2ba85df5c92e931
Sabuhi0 <sabuhiq0@gmail.com>
2022-05-13 22:58:35 +0400

    To do project

--- LearnReact/to-do-app/src/App.js
+++ LearnReact/to-do-app/src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import TodoList from './components/TodoList.jsx';
+import './App.css';
+
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
