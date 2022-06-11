4debaa9f67b683346213a9377c06bc18bcdd4583
sabuhiq0@gmail.com <sabuhiq0@gmail.com>
2022-06-11 12:54:32 +0400

    Check tailwind css

--- LearnReact/pixabay-gallery/src/App.jsx
+++ LearnReact/pixabay-gallery/src/App.jsx
@@ -1,9 +1,17 @@
+
+const formatName = ( [name, lastName, age] ) => {
+  return name + ' ' + lastName + " " + age;
+}
+
+const user = ['Sabuhi', "Gasimov", 17]
+
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+      <div className="App text-center text-red-600 text-[30px]">
+        <h1>Salam, {formatName(user)}</h1>
+      </div>
   );
 }
+
 
 export default App;
