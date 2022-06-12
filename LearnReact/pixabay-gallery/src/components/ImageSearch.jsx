e97d7d160910182c9d3115bf1b5f92723e1b20e9
sabuhiq0@gmail.com <sabuhiq0@gmail.com>
2022-06-12 13:14:16 +0400

    Update input

--- LearnReact/pixabay-gallery/src/components/ImageSearch.jsx
+++ LearnReact/pixabay-gallery/src/components/ImageSearch.jsx
@@ -12,7 +12,7 @@ const ImageSearch = ({ searchText }) => {
         <div className='max-s-sm rounded overflow-hidden py-10 mx-auto'>
             <form onSubmit={onSubmit} className='w-full flex justify-center'>
                 <div className='flex items-center border-b-2 border-teal-500 py-2'>
-                    <input onChange={e => setText(e.target.value)} className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type="text" placeholder='Search Image Term...' />
+                    <input onChange={e => setText(e.target.value)} className='appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none' type="text" placeholder='Search Image Term...' />
                     <button className='flex-shrink-0 bg-[#20C997] border-[#20C997] hover:border-[#1CAD82] hover:bg-[#1CAD82] text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Search</button>
                 </div>
             </form>
