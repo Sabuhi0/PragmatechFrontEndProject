189681c1ede050e8693c9e7d20d087a88fd2aee7
Sabuhi0 <sabuhiq0@gmail.com>
2022-06-23 22:21:47 +0400

    Add Card component

--- TypeScriptReact/Shopping-Cart/src/components/shopping/Card.tsx
+++ TypeScriptReact/Shopping-Cart/src/components/shopping/Card.tsx
@@ -6,11 +6,10 @@ import Items from "./Items";
 const Card = () => {
     const { data } = useContext<Icard>(CardContext)
     return (
-        <div className="flex flex-col space-y-3 m-4">
+        <div className="grid grid-cols-3 gap-5 m-4">
             { data.map(data => <Items key={data.id} data = {data} />) }
         </div>
     )
 }
 
 export default Card;
- 
