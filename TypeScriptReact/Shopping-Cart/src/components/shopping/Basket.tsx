1ce23702d6fd10d2e96ba157eeb2ef14bbf216f2
Sabuhi0 <sabuhiq0@gmail.com>
2022-06-23 22:23:02 +0400

    Update component

--- TypeScriptReact/Shopping-Cart/src/components/shopping/Basket.tsx
+++ TypeScriptReact/Shopping-Cart/src/components/shopping/Basket.tsx
@@ -6,9 +6,24 @@ const Basket = () => {
     const { card } = useContext(CardContext)
 
     return (
-        <div className='bg-gray-500'>
+        <div className='min-h-screen bg-gray-500'>
             {
-                card.map(s => <div key={s.id}>{s.price}$</div>)
+               card.length !== 0 ?  card.map(item => 
+                <div className='p-2 m-2 bg-white rounded-md'>
+                    <div className='flex items-center justify-start'>
+                        <div className=''>
+                            <img className='w-full' src={item.image} alt="" />
+                        </div>
+                        <div className='flex justify-center w-full'>
+                            <ul className='flex flex-col space-y-5'>
+                                <li className='text-[25px]'>Name: <span className='text-[20px] font-bold'>{item.name}</span></li>
+                                <li className='text-[25px]'>Size: <span className='text-[20px] font-bold'>{item.size}GB</span></li>
+                                <li className='text-[25px]'>Color: <span className='text-[20px] font-bold'>{item.color}</span></li>
+                                <li className='text-[25px]'>Price: <span className='text-[20px] font-bold'>{item.price}$</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>) : <h1 className='mt-2 text-5xl font-semibold text-center text-white'>The basket is empty</h1>
             }
         </div>
     )
