0f5e463899ae6c6d3b41bbcbb89b0d4d8b5dc5a9
Sabuhi0 <sabuhiq0@gmail.com>
2022-06-26 11:44:43 +0400

    Update card component

--- TypeScriptReact/Shopping-Cart/src/components/shopping/Card.tsx
+++ TypeScriptReact/Shopping-Cart/src/components/shopping/Card.tsx
@@ -6,7 +6,7 @@ import Items from "./Items";
 const Card = () => {
     const { data } = useContext<Icard>(CardContext)
     return (
-        <div className="grid grid-cols-3 gap-5 m-4">
+        <div className="grid grid-cols-2 gap-5 m-4">
             { data.map(data => <Items key={data.id} data = {data} />) }
         </div>
     )
