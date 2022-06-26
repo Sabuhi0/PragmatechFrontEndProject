92bc2277e88227f907b6c060b2d3331d114e1696
Sabuhi0 <sabuhiq0@gmail.com>
2022-06-26 11:45:13 +0400

    Update card button

--- TypeScriptReact/Shopping-Cart/src/components/shopping/Items.tsx
+++ TypeScriptReact/Shopping-Cart/src/components/shopping/Items.tsx
@@ -17,19 +17,16 @@ const Items = ({ data }: {data: ShoppingData}) => {
     }
 
     return (
-        <div className='flex justify-between px-5 bg-gray-300 border rounded-md shadow-lg h-[250px]'>
-        <div className='flex flex-col p-3 space-y-2'>
-            <img src={data.image} width={50} />
-            <h1>{data.name}</h1>
-            <h1>{data.size}GB</h1>
-            <h1>{data.price}$</h1>
-            <h1>{data.color}</h1> 
-        </div>
-        <div>
-            <button onClick={() => click(data.id)}>
-               { card.some(s => s.id === data.id) ? <IoMdRemoveCircle size={40} /> : <MdOutlineAdd size={40} /> }
-            </button>
-        </div>
+        <div className='flex justify-between px-5 bg-gray-300 border rounded-md shadow-lg h-[300px] overflow-hidden'>
+            <div className='flex flex-col p-3 space-y-2'>
+                <img src={data.image} className='w-[100%] h-full object-cover' />
+            </div>
+            <div>
+                <button className='border rounded p-3 mt-2 flex items-center hover:bg-slate-400 hover:text-white' onClick={() => click(data.id)}>
+                    {card.some(s => s.id === data.id) ? 'Remove from the card' : 'Add to the card'}
+                    <span className='pl-1'>{ card.some(s => s.id === data.id) ? <IoMdRemoveCircle size={20} /> : <MdOutlineAdd size={20} /> }</span>
+                </button>
+            </div>
         </div>
     )
 }
