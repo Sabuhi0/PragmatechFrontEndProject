f60b6dbac526209b49620c593bf5a84ff8e52777
Sabuhi0 <sabuhiq0@gmail.com>
2022-06-28 01:59:27 +0400

    Update items component

--- TypeScriptReact/Shopping-Cart/src/components/shopping/Items.tsx
+++ TypeScriptReact/Shopping-Cart/src/components/shopping/Items.tsx
@@ -17,12 +17,20 @@ const Items = ({ data }: {data: ShoppingData}) => {
     }
 
     return (
-        <div className='flex justify-between px-5 bg-gray-300 border rounded-md shadow-lg h-[300px] overflow-hidden'>
-            <div className='flex flex-col p-3 space-y-2'>
-                <img src={data.image} className='w-[100%] h-full object-cover' />
+        <div className='flex flex-col justify-between p-5 bg-gray-300 border rounded-md shadow-lg overflow-hidden'>
+            <div className='p-3 space-y-2'>
+                <img src={data.image} className='w-[100%]' />
             </div>
-            <div>
-                <button className='border rounded p-3 mt-2 flex items-center hover:bg-slate-400 hover:text-white' onClick={() => click(data.id)}>
+            <div className='pl-3'>
+                <ul>
+                    <li className='text-2xl'>Name: <span className='font-semibold'>{data.name}</span></li>
+                    <li className='text-2xl'>Size: <span className='font-semibold'>{data.size}GB</span></li>
+                    <li className='text-2xl'>Color: <span className='font-semibold'>{data.color}</span></li>
+                    <li className='text-2xl'>Price: <span className='font-semibold'>{data.price}$</span></li>
+                </ul>
+            </div>
+            <div className='flex justify-end'>
+                <button className='border rounded p-3 mt-2 flex items-center hover:bg-slate-400 hover:text-white transition-all' onClick={() => click(data.id)}>
                     {card.some(s => s.id === data.id) ? 'Remove from the card' : 'Add to the card'}
                     <span className='pl-1'>{ card.some(s => s.id === data.id) ? <IoMdRemoveCircle size={20} /> : <MdOutlineAdd size={20} /> }</span>
                 </button>
